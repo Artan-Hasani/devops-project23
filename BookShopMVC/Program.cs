9f4093a9a59d37f035ca4b919db58093116d3b1a
Artan-Hasani <100138330+Artan-Hasani@users.noreply.github.com>
2023-11-29 21:34:02 +0100

    AB#0002 Adding swagger (#3)

--- BookShopMVC/Program.cs
+++ BookShopMVC/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add Swagger services
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -11,6 +19,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint and Swagger UI
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 
 app.UseRouting();
 

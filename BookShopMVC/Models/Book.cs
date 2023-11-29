9d608361440133e1c669da685e22c063e3288a22
Artan-Hasani <100138330+Artan-Hasani@users.noreply.github.com>
2023-11-29 23:36:58 +0100

    AB#0004 Add Entity Framework and Postgres db

---
+++ BookShopMVC/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShopMVC.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+    }
+}
---
+++ BookShopMVC/Models/EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookShopMVC.Models.EntityFramework
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Define DbSets here. For example:
+        public DbSet<Book> Books { get; set; }
+    }
+}

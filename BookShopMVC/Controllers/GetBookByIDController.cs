232eb017c1db84e208d5720d34fcafc029edf2e8
Artan-Hasani <artanhasani999@gmail.com>
2023-12-02 18:55:44 +0100

    Improve Get and Create Book controllers and add GetBookByIDController

---
+++ BookShopMVC/Controllers/GetBookByIDController.cs
@@ -0,0 +1,22 @@
+﻿using BookShopMVC.Models.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopMVC.Controllers
+{
+    public class GetBookByIDController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public GetBookByIDController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("GetBookByID")]
+        public IActionResult GetBookByID([FromQuery] int Id)
+        {
+            var book = _dbContext.Books.FirstOrDefault(b => b.Id == Id);
+            return Ok(book);
+        }
+    }
+}
